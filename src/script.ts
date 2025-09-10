86fa80059b9bb348b67c6aa99e10bd67492d0c63
FilipHans <filip.hansen98@gmail.com>
2025-09-10 09:33:31 +0200

    first push

---
+++ src/script.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import  express  from "express";
+import dotenv from 'dotenv';
+import { ApolloServer } from "@apollo/server";
+import { Query } from 'mongoose'
+import { expressMiddleware } from "@as-integrations/express5";
+import connectDB from "./db";
+import resolvers from "./recipe/graphql/resolvers";
+import typeDefs from "./recipe/graphql/typeDefs";
+
+dotenv.config();
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+// Start and connect Apollo server
+const server = new ApolloServer({typeDefs, resolvers})
+server.start().then(e => {
+    app.use("/graphql", expressMiddleware(server, {
+    context: async () => ({}),
+}))
+
+})
+
+// Connect to MongdoDB
+connectDB()
+.then(e => console.log('connected to mongoDB'))
+.catch(error => {
+    console.error('Issue running server', error)
+})
+
+// Start server
+app.listen(port, () => {
+    console.log(`running on`, port)
+})
+
